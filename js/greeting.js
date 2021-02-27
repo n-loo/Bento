9ce94d7652300f3458226264b949017132ccd418
n-loo <nlvanderloo@gmail.com>
2021-02-27 00:34:11 -0500

    update

--- js/greeting.js
+++ js/greeting.js
@@ -4,6 +4,7 @@ const hour = today.getHours();
 
 // Here you can change your name
 const name = ' Niels';
+const name1 = ' Niels';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
@@ -15,11 +16,11 @@ const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = gree1 + name1;
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById('greetings').innerText = gree2 + name1;
 } else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
+  document.getElementById('greetings').innerText = gree3 + name1;
 } else {
-  document.getElementById('greetings').innerText = gree4 + name;
+  document.getElementById('greetings').innerText = gree4 + name1;
 }
